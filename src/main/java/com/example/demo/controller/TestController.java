f8ba71ae87d8db54e1dfe87480d63241016b7207
10747 <1074700762@qq.com>
2021-03-27 18:26:12 +0800

    上传

---
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -0,0 +1,21 @@
+package com.example.demo.controller;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class TestController {
+    private static Logger log = LoggerFactory.getLogger(TestController.class);
+    @GetMapping("/test")
+    public  String  test(){
+        log.info("2222222222请求成功22222222！");
+        return "2222222hello2222222";
+    }
+
+}
