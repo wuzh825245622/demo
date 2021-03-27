b46e2d817088c9d7b137e348bcd97a397abeb707
10747 <1074700762@qq.com>
2021-03-27 20:12:56 +0800

    上传

--- src/main/java/com/example/demo/controller/TestController.java
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -15,7 +15,7 @@ public class TestController {
     @GetMapping("/test")
     public  String  test(){
         log.info("2222222222请求成功22222222！");
-        return "2222222hello2222222";
+        return "2222222hello22222221";
     }
 
 }
